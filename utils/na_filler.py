84629d7f4a79bc14cd6293fa0df9b3375daa448e
Benjamin Cerf <benjamincerfpro@gmail.com>
2024-10-30 12:48:55 +0100

    changed imputers with nb_days parameter

--- utils/na_filler.py
+++ utils/na_filler.py
@@ -8,19 +8,41 @@ from qolmat.imputations import imputers
 from qolmat.utils import data, plot
 from sklearn.linear_model import LinearRegression
 
-def Imputer_MICE(df, columns_to_impute):
-    
+def Imputer_MICE(df, columns_to_impute, nb_days=None):
+        
+    if nb_days is None:
+        nb_transform = df.shape[0]
+    else:
+        nb_transform = nb_days*24
+
+    df = df.tail(nb_transform)
+
+    # Filtre les colonnes pour ne garder que celles numériques
+    df_numeric = df[columns_to_impute].select_dtypes(include=[np.number])
+
+
     imputer_mice = imputers.ImputerMICE(
     estimator=LinearRegression(),
     sample_posterior=False,
     max_iter=100
     )
 
-    df[columns_to_impute] = imputer_mice.fit_transform(df[columns_to_impute])
+    imputer_mice.fit(df_numeric)
+    df[columns_to_impute] = imputer_mice.transform(df[columns_to_impute])
 
     return df
 
-def Imputer_TSOU(df, columns_to_impute):
+def Imputer_TSOU(df, columns_to_impute, nb_days=None):
+    
+    if nb_days is None:
+        nb_transform = df.shape[0]
+    else:
+        nb_transform = nb_days*24
+
+    df = df.tail(nb_transform)
+
+    # Filtre les colonnes pour ne garder que celles numériques
+    df_numeric = df[columns_to_impute].select_dtypes(include=[np.number])
 
     imputer_tsou = imputers.ImputerEM(
     model="VAR",
@@ -30,7 +52,8 @@ def Imputer_TSOU(df, columns_to_impute):
     dt=1e-3,
     p=1)
 
-    df[columns_to_impute] = imputer_tsou.fit_transform(df[columns_to_impute])
+    imputer_tsou.fit(df_numeric)
+    df[columns_to_impute] = imputer_tsou.transform(df[columns_to_impute])
 
     return df
     
