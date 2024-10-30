a341f94012281c132c6c37f42b31cb0de03eaa29
Matthieu Delsart <matthieu.delsart@hec.edu>
2024-10-30 22:48:33 +0100

    update tft

--- utils/lagger.py
+++ utils/lagger.py
@@ -24,3 +24,27 @@ def create_lags(df, columns, max_lag):
         for lag in range(1, max_lag + 1):
             df_lagged[f"{col}_lag_{lag}"] = df[col].shift(lag)
     return df_lagged
+
+def create_lags_advanced(df, lag_dict={}):
+    """
+    Adds lag columns to a DataFrame for a given list of columns.
+
+    Parameters:
+    -----------
+    df : pd.DataFrame
+        Origine dataframe
+    columns : list
+        List of columns for which we want to add lags.
+    max_lag : int
+        Max number of lags to create.
+
+    Returns:
+    --------
+    pd.DataFrame
+        DataFrame lag columns.
+    """
+    df_lagged = df.copy()
+    for col in lag_dict:
+        for lag in lag_dict[col]:
+            df_lagged[f"{col}_lag_{lag}"] = df[col].shift(lag)
+    return df_lagged
